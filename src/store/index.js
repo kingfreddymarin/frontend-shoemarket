ce50c6fbdf169dbee6e8ece578f4ea07c3423847
Carlos Torres <appleuser@Carlos-Torres-MacBook-Pro.local>
2022-12-01 10:03:34 -0600

    Correcion redux

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import cartSlice from './cart-slice';
 
 
 const store = configureStore({
-  reducer: { counter: cartSlice.reducer}  //Behind the scene, it will merge all the reducer into one big reducer
+  reducer: { cart: cartSlice.reducer}  //Behind the scene, it will merge all the reducer into one big reducer
 });
 
 export const cartActions = cartSlice.actions;
